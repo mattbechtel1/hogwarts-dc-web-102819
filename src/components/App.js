c788254650cc25d7a821e8c8419d751ea7a85768
mattbechtel1 <bechma02@gmail.com>
2020-01-08 16:39:51 -0500

    app built and mostly stable

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,31 @@ import '../App.css';
 import Header from './Header'
 import hogs from '../porkers_data';
 import PorkersContainer from './PorkersContainer';
+import Options from './Options';
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      greasedOnly : false,
+      sort : "default"
+    }
+  }
+
+  changeState = (e) => {
+    this.setState({
+      [e.currentTarget.name]: e.currentTarget.value
+    })
+  }
+
+
+
   render() {
     return (
       <div className="App">
           < Header />
-          < PorkersContainer hogs={hogs} />
+          < Options changeState={this.changeState}/>
+          < PorkersContainer hogs={hogs} displayOption = {this.state} />
       </div>
     )
   }
